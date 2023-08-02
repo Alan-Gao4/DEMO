578a29dafc2fd3b1d4b77098457404891fc8f1f8
Alan-Gao4 <139179478+Alan-Gao4@users.noreply.github.com>
2023-08-02 14:26:07 -0700

    Add files via upload

---
+++ story_time.js
@@ -0,0 +1,33 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+var name = "Laplus"
+var title = "Demon of Darkness"
+var worldname = "Comedia"
+var activity = " make videos "
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+var year = 1500
+var subscribers = 1000000
+var century = 16
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+var employees = ["Secretary", "Janitor", "Scientist", "Military"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+var boolean = true
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Welcome to " + worldname + ". I am " + name + ", " + title)
+console.log("I, along with many others, " + activity + " for the entertainment of the citizens of " + worldname)
+console.log("It is the year " + year + " , " + century + "th century")
+console.log("In this world, your worth is based on fame. I for one have " + subscribers + " subscribers. ")
+console.log("Tourist: I have a question oh great Demon of Darkness")
+console.log("A question? Ask away. ")
+console.log("Tourist: Does this mean that you are famous?")
+console.log(boolean + ", this means I am quite famous, but it was not achieved without the help of my \n" + employees[0] + " and many others. ")
+console.log("Feel free to stay as long as you like. ")
